befa65be4aa4f224b4e61dd6d3705b7aa6f8357b
aleksandr gorbach <alex@aleksandrs-Air.fios-router.home>
2022-03-04 11:35:55 -0500

    nav debugging

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+// import Home from './components';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {/* <CssBaseline /> */}
+      <Navbar />
+      <Routes>
+        {/* <Route exact path="/" component={Home} /> */}
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/resume" src="https://drive.google.com/file/d/1IpIh8hEJy-IuiTbPDASb-8-GKltqxh_x/view" />
+        {/* <Route exact path="/contact" component={Contact} /> */}
+      </Routes>
+    </React.Fragment>
   );
 }
 
