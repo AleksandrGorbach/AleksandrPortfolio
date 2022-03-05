886ba333987d97b774550fc3c6fcf142e224b059
aleksandr gorbach <alex@aleksandrs-Air.fios-router.home>
2022-03-04 19:40:46 -0500

    header

--- portfolio/src/components/index.js
+++ portfolio/src/components/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import Header from './Header';
+import Header from './Header';
 import Particles from './Particles';
 
 const Home = () => {
     return (
         <React.Fragment>
-            {/* <Header /> */}
+            <Header />
             <Particles />
         </React.Fragment>
     );
